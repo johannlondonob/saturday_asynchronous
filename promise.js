a09fc6663d9e41bf92c56425b6a62b68213304ee
Johan Alexander Londoño Bedoya <johannlondonob@gmail.com>
2021-09-11 17:02:16 -0500

    added promise example

---
+++ promise.js
@@ -0,0 +1,27 @@
+let loginNetflix = ( authentication ) => {
+    console.log('Autenticando usuario...')
+    return new Promise( function ( resolve, reject ) {
+        setTimeout( () => {
+            if (authentication.email === 'johannlondonob@gmail.com' && authentication.password === '1234') {
+                console.log( 'Autenticación exitosa' )
+                resolve( 200 )
+            } else {
+                console.log( 'Autenticación fallida...' )
+                reject( 400 )
+            }
+        }, 5000 )
+    } )
+}
+
+user = {
+    email: 'johannlondonob@gmail.com',
+    password: '1234'
+}
+
+loginNetflix( user )
+    .then( ( response ) => {
+        console.log( response )
+    } )
+    .catch((error) => {
+        console.log(error)
+    })
